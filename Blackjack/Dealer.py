361a2ad1dd941c4c15d58d25ffb1ba800d0b06b9
shappadappa <michael.farrugia5500@outlook.com>
2025-05-26 14:04:03 +0200

    finished project

--- Blackjack/Dealer.py
+++ Blackjack/Dealer.py
@@ -2,8 +2,7 @@ from Blackjack.Action import Action
 
 class Dealer():
     def get_policy(self, sum):
-        # Assuming dealer stands at 21
-        if sum < 17 and sum <= 21:
+        if sum < 17:
             return Action.HIT
         else:
             return Action.STAND
